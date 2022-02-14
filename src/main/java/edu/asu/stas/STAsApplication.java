e23c343baab304fff6a0bfd3a7250e21cfd7beeb
Saif Aqqad <saifo1999@gmail.com>
2022-02-14 10:01:12 +0200

    Initialize project

---
+++ src/main/java/edu/asu/stas/STAsApplication.java
@@ -0,0 +1,13 @@
+package edu.asu.stas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STAsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(STAsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/asu/stas/STAsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.asu.stas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STAsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
