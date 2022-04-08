5f62ed87dac31d4ee25199186a9dc21eaeb7bc60
Mustafa <d50dv4@gmail.com>
2022-04-08 06:07:41 -0700

    create page

--- src/main/java/edu/asu/stas/controllers/ProfileController.java
+++ src/main/java/edu/asu/stas/controllers/ProfileController.java
@@ -6,8 +6,8 @@ import edu.asu.stas.service.StudentProfileService;
 import edu.asu.stas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Objects;
 
@@ -21,14 +21,19 @@ public class ProfileController {
         this.studentProfileService = studentProfileService;
     }
 
-    @ModelAttribute("studentProfile")
     public StudentProfile getStudentProfile(){
         User user = Objects.requireNonNull(UserService.getAuthenticatedUser());
         return studentProfileService.getProfileByUser(user);
     }
 
     @GetMapping("/profile")
-    public String getProfile(){
-        return "profile/index";
+    public String getProfile(Model model){
+        StudentProfile profile = getStudentProfile();
+        if(Objects.nonNull(profile)){
+            model.addAttribute("profile", profile);
+            return "profile/index";
+        }else{
+            return "profile/create";
+        }
     }
 }
