368b7c34f79fd329057d256e2b8af78927de5ce5
Saif Aqqad <saifo1999@gmail.com>
2022-05-06 10:24:09 +0300

    Refactor RestUtils

--- src/main/java/edu/asu/stas/lib/RestUtils.java
+++ src/main/java/edu/asu/stas/lib/RestUtils.java
@@ -1,13 +1,14 @@
 package edu.asu.stas.lib;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Objects;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RestUtils {
-    private RestUtils() {
-    }
 
     public static <T> T requireNonNull(T o) {
         if (Objects.isNull(o)) {
