675f2bdcc52249443c0f4f35620f8701d63d5ab0
Saif Aqqad <saifo1999@gmail.com>
2022-06-04 09:02:04 +0300

    Fix LoginController not redirecting users to previous request

--- src/main/java/edu/asu/stas/login/LoginController.java
+++ src/main/java/edu/asu/stas/login/LoginController.java
@@ -9,6 +9,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,10 +22,12 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
 @Controller
-@SessionAttributes({"tempAuthentication"})
+@SessionAttributes({"tempAuthentication", "redirectUri"})
 public class LoginController {
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
@@ -41,72 +45,91 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String getLoginPage(Authentication authentication, SessionStatus sessionStatus) {
-        if (authentication != null && authentication.isAuthenticated())
+    public String getLoginPage(
+        Authentication authentication,
+        Model model,
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) {
+        if (authentication != null && authentication.isAuthenticated()) {
             return "redirect:/";
-        sessionStatus.setComplete();
+        }
+        model.addAttribute("redirectUri", getRedirectUri(request, response));
         return "login/index";
     }
 
     @PostMapping("/login/do")
     public String postLoginForm(
-            @Validated LoginForm loginForm,
-            BindingResult bindingResult,
-            RedirectAttributes redirectAttributes,
-            SessionStatus sessionStatus
+        @Validated LoginForm loginForm,
+        BindingResult bindingResult,
+        Model model,
+        RedirectAttributes redirectAttributes,
+        SessionStatus sessionStatus
     ) {
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.loginForm", bindingResult);
+            redirectAttributes.addFlashAttribute(
+                "org.springframework.validation.BindingResult.loginForm",
+                bindingResult
+            );
             redirectAttributes.addFlashAttribute("loginForm", loginForm);
             return "redirect:/login?error";
         }
-        return doLogin(loginForm, redirectAttributes, sessionStatus);
+        return doLogin(loginForm, model, redirectAttributes, sessionStatus);
     }
 
-    private String doLogin(LoginForm loginForm, RedirectAttributes redirectAttributes, SessionStatus sessionStatus) {
+    private String doLogin(
+        LoginForm loginForm,
+        Model model,
+        RedirectAttributes redirectAttributes,
+        SessionStatus sessionStatus
+    ) {
         Authentication authentication;
         try {
             authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginForm.getEmail(),
-                            loginForm.getPassword()
-                    )
+                new UsernamePasswordAuthenticationToken(
+                    loginForm.getEmail(),
+                    loginForm.getPassword()
+                )
             );
-        } catch (AuthenticationException e) {
+        } catch (AuthenticationException | IllegalArgumentException e) {
             redirectAttributes.addFlashAttribute("authError", true);
-            if(e instanceof DisabledException)
+            if (e instanceof DisabledException) {
                 redirectAttributes.addFlashAttribute("errorType", 1);
+            }
             return "redirect:/login?error";
         }
         if (isUsing2FA(authentication)) {
             redirectAttributes.addFlashAttribute("tempAuthentication", authentication);
             return "redirect:/login/2fa";
         }
+        String redirectUri = (String) model.getAttribute("redirectUri");
         sessionStatus.setComplete();
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return "redirect:/";
+        return "redirect:" + redirectUri;
     }
 
     @GetMapping("/login/2fa")
     public String get2FAPage(Model model) {
-        if (!model.containsAttribute("tempAuthentication"))
+        if (!model.containsAttribute("tempAuthentication")) {
             return "redirect:/login";
+        }
         return "login/2fa";
     }
 
     @PostMapping("/login/2fa/do")
     public String post2FA(
-            String code,
-            Model model,
-            SessionStatus sessionStatus,
-            RedirectAttributes redirectAttributes
+        String code,
+        Model model,
+        SessionStatus sessionStatus,
+        RedirectAttributes redirectAttributes
     ) {
         Authentication tempAuth = (Authentication) model.getAttribute("tempAuthentication");
         User user = Objects.requireNonNull(getUser(tempAuth));
         if (userService.is2FACodeValid(user, code)) {
+            String redirectUri = (String) model.getAttribute("redirectUri");
             sessionStatus.setComplete();
             SecurityContextHolder.getContext().setAuthentication(tempAuth);
-            return "redirect:/";
+            return "redirect:" + redirectUri;
         } else {
             redirectAttributes.addFlashAttribute("tempAuthentication", tempAuth);
             redirectAttributes.addFlashAttribute("auth2FAError", true);
@@ -117,10 +140,11 @@ public class LoginController {
     private boolean isUsing2FA(Authentication authentication) {
         if (Objects.nonNull(authentication) && authentication.isAuthenticated()) {
             Object principle = authentication.getPrincipal();
-            if (principle instanceof User user)
+            if (principle instanceof User user) {
                 return user.isUsing2FA();
-            else if (principle instanceof Connection connection)
+            } else if (principle instanceof Connection connection) {
                 return connection.getUser().isUsing2FA();
+            }
         }
         return false;
     }
@@ -128,12 +152,17 @@ public class LoginController {
     private User getUser(Authentication authentication) {
         if (Objects.nonNull(authentication) && authentication.isAuthenticated()) {
             Object principle = authentication.getPrincipal();
-            if (principle instanceof User user)
+            if (principle instanceof User user) {
                 return user;
-            else if (principle instanceof Connection connection)
+            } else if (principle instanceof Connection connection) {
                 return connection.getUser();
+            }
         }
         return null;
     }
 
+    private String getRedirectUri(HttpServletRequest request, HttpServletResponse response) {
+        var savedRequest = (DefaultSavedRequest) (new HttpSessionRequestCache().getRequest(request, response));
+        return Objects.nonNull(savedRequest) ? savedRequest.getRequestURI() : "/";
+    }
 }
