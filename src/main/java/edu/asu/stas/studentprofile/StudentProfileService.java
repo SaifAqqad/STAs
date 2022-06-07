204b6ce78e42b2bcfb6090bae9c7ac752610076b
Saif Aqqad <saifo1999@gmail.com>
2022-06-07 10:43:49 +0300

    Fix createProfile functionality

--- src/main/java/edu/asu/stas/studentprofile/StudentProfileService.java
+++ src/main/java/edu/asu/stas/studentprofile/StudentProfileService.java
@@ -128,41 +128,81 @@ public class StudentProfileService {
 
     @Transactional
     public StudentProfile createProfile(StudentProfile profile) {
-        profile.getCourses().forEach(this::processCourse);
-        profile.getActivities().forEach(this::processActivity);
-        profile.getProjects().forEach(this::processProject);
-        profile.setUser(UserService.getAuthenticatedUser());
-        profile = studentProfileRepository.save(profile);
-        return profile;
+        StudentProfile initialProfile = new StudentProfile();
+        initialProfile.setUser(UserService.getAuthenticatedUser());
+        initialProfile.setName(profile.getName());
+        initialProfile.setAbout(profile.getAbout());
+        initialProfile.setMajor(profile.getMajor());
+        initialProfile.setLocation(profile.getLocation());
+        initialProfile.setUniversity(profile.getUniversity());
+        initialProfile.setContactEmail(profile.getContactEmail());
+        initialProfile.setContactPhone(profile.getContactPhone());
+        StudentProfile savedProfile = studentProfileRepository.save(initialProfile);
+        savedProfile.getCourses().addAll(profile.getCourses());
+        savedProfile.getCourses().forEach(course -> this.processCourse(course, savedProfile));
+        savedProfile.getActivities().addAll(profile.getActivities());
+        savedProfile.getActivities().forEach(activity -> this.processActivity(activity, savedProfile));
+        savedProfile.getProjects().addAll(profile.getProjects());
+        savedProfile.getProjects().forEach(project -> this.processProject(project, savedProfile));
+        savedProfile.getExperiences().addAll(profile.getExperiences());
+        initialProfile = studentProfileRepository.save(savedProfile);
+        return initialProfile;
     }
 
-    private void processCourse(Course course) {
+    private void processCourse(Course course, StudentProfile profile) {
+        course.setProfile(profile);
         if (course.getImageUri() != null) {
+            var courseImage = course.getImageUri();
+            course.setImageUri(null);
+            course = courseRepository.save(course);
             try {
-                course.setImageUri(contentService.storeResource(course.getImageUri(), "course", course.getId().toString()));
+                course.setImageUri(contentService.storeResource(
+                    courseImage,
+                    "course",
+                    course.getId().toString()
+                ));
             } catch (IOException e) {
                 course.setImageUri(null);
             }
         }
+        courseRepository.save(course);
     }
 
-    private void processActivity(Activity activity) {
+    private void processActivity(Activity activity, StudentProfile profile) {
+        activity.setProfile(profile);
         if (activity.getImageUri() != null) {
+            var activityImage = activity.getImageUri();
+            activity.setImageUri(null);
+            activity = activityRepository.save(activity);
             try {
-                activity.setImageUri(contentService.storeResource(activity.getImageUri(), "activity", activity.getId().toString()));
+                activity.setImageUri(contentService.storeResource(
+                    activityImage,
+                    "activity",
+                    activity.getId().toString()
+                ));
             } catch (IOException e) {
                 activity.setImageUri(null);
             }
         }
+        activityRepository.save(activity);
     }
 
-    private void processProject(Project project) {
+    private void processProject(Project project, StudentProfile profile) {
+        project.setProfile(profile);
         if (project.getImageUri() != null) {
+            var projectImage = project.getImageUri();
+            project.setImageUri(null);
+            project = projectRepository.save(project);
             try {
-                project.setImageUri(contentService.storeResource(project.getImageUri(), "project", project.getId().toString()));
+                project.setImageUri(contentService.storeResource(
+                    projectImage,
+                    "project",
+                    project.getId().toString()
+                ));
             } catch (IOException e) {
                 project.setImageUri(null);
             }
         }
+        projectRepository.save(project);
     }
 }
