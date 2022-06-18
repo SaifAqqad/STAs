f4c3f0135ef6389db724cfc53d434b75d18624d9
Saif Aqqad <saifo1999@gmail.com>
2022-06-18 22:28:33 +0300

    Remove home page

--- src/main/java/edu/asu/stas/shared/HomeController.java
+++ src/main/java/edu/asu/stas/shared/HomeController.java
@@ -1,14 +1,19 @@
 package edu.asu.stas.shared;
 
+import edu.asu.stas.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class HomeController  {
+public class HomeController {
 
     @GetMapping("/")
-    public String getHomePage() {
-        return "home/index";
+    public String getHomePage(@ModelAttribute User authenticatedUser) {
+        if (authenticatedUser != null) {
+            return "redirect:/profile";
+        }
+        return "redirect:/login";
     }
 
 }
